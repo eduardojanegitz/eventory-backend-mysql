399835e3c93be0dfcdef36718b89a63c679b5d7b
Eduardo Alves <edu0alves00@gmail.com>
2023-12-14 21:27:39 -0300

    alterado o centro de custo

--- controllers/costController.js
+++ controllers/costController.js
@@ -1,40 +1,45 @@
-import Cost from "../models/Cost.js";
+import * as Cost from "../models/Cost.js";
 
 export const getAllCost = async (req, res) => {
   try {
-    const getCost = await Cost.find();
-
-    res.status(200).json(getCost);
-  } catch (error) {}
+    const getCostCenter = await Cost.getAllCostCenter();
+    res.status(200).json(getCostCenter);
+  } catch (error) {
+    console.error("Erro ao buscar os centros de custo:", error);
+    res.status(500).json({ message: "Erro interno do servidor." });
+  }
 };
 
 export const createCost = async (req, res) => {
   try {
-    const { description, code, active } = req.body;
-    const existCost = await Cost.findOne({ code });
-    if (existCost) {
-      res
-        .status(400)
-        .json({ error: "Já existe um centro de custo com esse código" });
+    const { name, description } = req.body;
+
+    if (!name || !description) {
+      return res.status(400).json({ message: "Campos obrigatórios ausentes." });
     }
-    const response = await Cost.create({ description, code, active });
-    res
-      .status(201)
-      .json({ response, msg: "Centro de custo criado com sucesso!" });
+
+    await Cost.createCostCenter({ name, description });
+    res.status(201).json({ msg: "Centro de custo criado com sucesso!" });
   } catch (error) {
-    res.status(500).json({ error: error });
+    res.status(500).json("Erro interno no servidor: ", error);
+    console.error("Erro ao criar o centro de custo: ", error);
   }
 };
 
 export const updateCost = async (req, res) => {
   try {
-    const { description, active } = req.body;
+    const { name, description, active } = req.body;
     const { id } = req.params;
-    const cost = await Cost.findById(id).exec();
-    if (!cost) {
-      res.status(204).json({ msg: `Nenhum centro de custo com esse ID` });
-    }
-    const response = await cost.updateOne({ description, active });
+    // const cost = await Cost.findById(id).exec();
+    // if (!cost) {
+    //   res.status(204).json({ msg: `Nenhum centro de custo com esse ID` });
+    // }
+    const response = {
+      name,
+      description,
+      active,
+    };
+    await Cost.updateCostCenter(id, response);
     res
       .status(201)
       .json({ response, msg: "Centro de custo atualizado com sucesso!" });
--- models/Cost.js
+++ models/Cost.js
@@ -1,23 +1,75 @@
 import mongoose from "mongoose";
+import db from "../config/db.js";
 
-const CostSchema = new mongoose.Schema(
-  {
-    code: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    description: {
-      type: String,
-      required: true,
-    },
-    active: {
-      type: String,
-      default: true,
-    },
-  },
-  { timestamps: true }
-);
+export const getAllCostCenter = async () => {
+  const [costCenter] = await db.query(
+    "SELECT CUSTO_IN_ID, CUSTO_ST_NOME, CUSTO_ST_DESCRICAO, CUSTO_CH_ATIVO, CUSTO_DT_CRIACAO FROM GLO_CENTRO_CUSTO"
+  );
+  return costCenter;
+};
 
-const Cost = mongoose.model("Cost", CostSchema);
-export default Cost;
+export const createCostCenter = async (costCenterData) => {
+  const { name, description } = costCenterData;
+
+  const [response] = await db.query(
+    "INSERT INTO GLO_CENTRO_CUSTO (CUSTO_ST_NOME, CUSTO_ST_DESCRICAO) VALUES (?,?)",
+    [name, description]
+  );
+  return response;
+};
+
+export const updateCostCenter = async (id, costCenterData) => {
+  const {name, description, active} = costCenterData;
+
+  const [costCenter] = await db.query(
+    "SELECT CUSTO_ST_NOME, CUSTO_ST_DESCRICAO, CUSTO_CH_ATIVO FROM GLO_CENTRO_CUSTO WHERE CUSTO_IN_ID = ?",
+    [id]
+  );
+
+  if (costCenter.length === 0) {
+    throw new Error("Nenhum centro de custo encontrado com esse ID!");
+  }
+  const updateFields = [];
+  const updateValues = [];
+
+  if (name !== undefined) {
+    updateFields.push("CUSTO_ST_NOME = ?");
+    updateValues.push(branch);
+  }
+
+  if (description !== undefined) {
+    updateFields.push("CUSTO_ST_DESCRICAO = ?");
+    updateValues.push(description);
+  }
+
+  if (active !== undefined) {
+    updateFields.push("CUSTO_CH_ATIVO = ?");
+    updateValues.push(active);
+  }
+
+  const updateQuery =
+    "UPDATE GLO_CENTRO_CUSTO SET " + updateFields.join(", ") + " WHERE CUSTO_IN_ID = ?";
+
+  const [response] = await db.query(updateQuery, [...updateValues, id]);
+
+  return response;
+}
+
+
+export const deleteCostCenter = async (id) => {
+  const [costCenter] = await db.query(
+    "SELECT CUSTO_ST_NOME, CUSTO_ST_DESCRICAO, CUSTO_CH_ATIVO FROM GLO_CENTRO_CUSTO WHERE CUSTO_IN_ID = ?",
+    [id]
+  );
+
+  if (costCenter.length === 0) {
+    throw new Error("Nenhum centro de custo encontrado com esse ID!");
+  }
+
+  const [response] = await db.query(
+    "DELETE FROM GLO_CENTRO_CUSTO WHERE CUSTO_IN_ID = ?",
+    [id]
+  );
+
+  return response;
+};
