8c205c500e13d5b696940472b0f2df4b833c7a2d
Eduardo Alves <edu0alves00@gmail.com>
2023-12-23 19:10:21 -0300

    feat: Alterado a lógica de autenticação

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,4 @@
-import * as User from "../models/User.js";
+import db from "../config/db.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
@@ -6,49 +6,50 @@ export const login = async (req, res) => {
   const { username, password } = req.body;
 
   if (!username || !password) {
-    return res
-      .status(400)
-      .json({ message: "Usuário e senha são obrigatórios" });
+    return res.status(400).json({ message: "Usuário e senha são obrigatórios" });
   }
-  try {
-    const foundUser = await User.findOne({ username }).exec();
 
-    if (!foundUser || !foundUser.active) {
+  try {
+    const [rows] = await db.query('SELECT * FROM GLO_USUARIOS WHERE USUARIO_ST_ALTERNATIVO = ?', [username]);
+    const foundUser = rows[0];
+
+    if (!foundUser || !foundUser.USUARIO_CH_ATIVO) {
       return res.status(401).json({ message: "Usuário ou senha incorretos" });
     }
 
-    const match = await bcrypt.compare(password, foundUser.password);
+    const match = await bcrypt.compare(password, foundUser.USUARIO_ST_SENHA);
 
     if (!match) {
       return res.status(401).json({ message: "Usuário ou senha incorretos" });
     }
-    const roles = Object.values(foundUser.roles).filter(Boolean);
+
+    const roles = foundUser.USUARIO_ST_PERMISSAO;
     const accessToken = jwt.sign(
       {
         UserInfo: {
-          username: foundUser.username,
-          roles: roles,
+          username: foundUser.USUARIO_ST_ALTERNATIVO,
+          roles: foundUser.USUARIO_ST_PERMISSAO,
         },
       },
       process.env.ACCESS_TOKEN_SECRET,
       { expiresIn: "15m" }
-      );
-      
-      const refreshToken = jwt.sign(
-        { username: foundUser.username },
-        process.env.REFRESH_TOKEN_SECRET,
-        { expiresIn: "7d" }
-        );
-        
-    // Create secure cookie with refresh token
+    );
+    
+    const refreshToken = jwt.sign(
+      { username: foundUser.USUARIO_ST_ALTERNATIVO },
+      process.env.REFRESH_TOKEN_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    // Criar cookie seguro com refresh token
     res.cookie("jwt", refreshToken, {
-      httpOnly: true, //accessible only by web server
-      secure: true, //https
-      sameSite: "None", //cross-site cookie
-      maxAge: 7 * 24 * 60 * 60 * 1000, //cookie expiry: set to match rT
+      httpOnly: true, // acessível apenas pelo servidor web
+      secure: true, // https
+      sameSite: "None", // cookie entre sites
+      maxAge: 7 * 24 * 60 * 60 * 1000, // expiração do cookie: configurada para coincidir com o refreshToken
     });
 
-    // Send accessToken containing username and roles
+    // Enviar accessToken contendo nome de usuário e papéis
     res.json({ accessToken, roles });
   } catch (error) {
     console.error(error);
@@ -63,52 +64,46 @@ export const refresh = (req, res) => {
 
   const refreshToken = cookies.jwt;
 
-  jwt.verify(
-    refreshToken,
-    process.env.REFRESH_TOKEN_SECRET,
-    async (err, decoded) => {
-      if (err) return res.status(403).json({ message: "Forbidden" });
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, async (err, decoded) => {
+    if (err) return res.status(403).json({ message: "Proibido" });
 
-      const foundUser = await User.findOne({
-        username: decoded.username,
-      }).exec();
+    const [rows] = await db.query('SELECT * FROM GLO_USUARIOS WHERE USUARIO_ST_ALTERNATIVO = ?', [decoded.username]);
+    const foundUser = rows[0];
 
-      if (!foundUser) return res.status(401).json({ message: "Unauthorized" });
+    if (!foundUser) return res.status(401).json({ message: "Não autorizado" });
 
-      const roles = Object.values(foundUser.roles);
-      const accessToken = jwt.sign(
-        {
-          UserInfo: {
-            username: foundUser.username,
-            roles: roles,
-          },
+    const roles = foundUser.USUARIO_ST_PERMISSAO;
+    const accessToken = jwt.sign(
+      {
+        UserInfo: {
+          username: foundUser.USUARIO_ST_ALTERNATIVO,
+          roles: roles,
         },
-        process.env.ACCESS_TOKEN_SECRET,
-        { expiresIn: "15m" }
-      );
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "15m" }
+    );
 
-      res.json({ accessToken, roles });
-    }
-  );
+    res.json({ accessToken, roles });
+  });
 };
 
 export const logout = (req, res) => {
   const cookies = req.cookies;
-  if (!cookies?.jwt) return res.sendStatus(204); //No content
+  if (!cookies?.jwt) return res.sendStatus(204); // Sem conteúdo
   res.clearCookie("jwt", { httpOnly: true, sameSite: "None", secure: true });
-  res.json({ message: "Cookie cleared" });
+  res.json({ message: "Cookie removido" });
 };
-
 
 export const logged = (req, res) => {
   try {
     const token = req.cookies.jwt;
-    if(!token) return res.json(false)
+    if (!token) return res.json(false);
 
-    jwt.verify(token, process.env.REFRESH_TOKEN_SECRET)
+    jwt.verify(token, process.env.REFRESH_TOKEN_SECRET);
 
-    res.send(true)
+    res.send(true);
   } catch (error) {
-    res.json(false)
+    res.json(false);
   }
-}
+};
