1867c9c2dd26909a9e97cfea5ea3cc393016379f
Eduardo Alves <edu0alves00@gmail.com>
2023-12-27 22:48:33 -0300

    feat: Ajustando a organização do código de grupo de itens

--- controllers/itemGroupController.js
+++ controllers/itemGroupController.js
@@ -2,11 +2,16 @@ import * as ItemGroup from "../models/ItemGroup.js";
 
 export const getAllItemGroup = async (req, res) => {
   try {
-    const getItemGroup = await ItemGroup.getAllItemGroup();
+    const itemGroup = await ItemGroup.getAllItemGroup();
 
-    res.status(200).json(getItemGroup);
+    if (itemGroup.length === 0) {
+      return res.status(204);
+    }
+
+    res.status(200).json(itemGroup);
   } catch (error) {
-    console.log("Erro ao encontrar os grupos de itens", error);
+    console.log("Erro ao encontrar os grupos de itens: ", error);
+    res.status(500).json({ error: error.message });
   }
 };
 
@@ -14,23 +19,32 @@ export const createItemGroup = async (req, res) => {
   try {
     const { name, description, depreciation } = req.body;
 
-    if (!name || !description || !depreciation) {
-      return res.status(400).json({ message: "Campos obrigatórios ausentes." });
+    const requiredFields = ["name", "description", "depreciation"];
+    const missingFields = [];
+
+    requiredFields.forEach((field) => {
+      if (!req.body[field]) {
+        missingFields.push(field);
+      }
+    });
+
+    if (missingFields.length > 0) {
+      return res.status(400).json({
+        error: `Campos obrigatórios ausentes: ${missingFields.join(", ")}`,
+      });
     }
 
     const itemGroup = {
       name,
       description,
-      depreciation
+      depreciation,
     };
 
     await ItemGroup.createItemGroup(itemGroup);
-    res
-      .status(201)
-      .json({msg: "Grupo de itens criado com sucesso!" });
+    res.status(201).json({ msg: "Grupo de itens criado com sucesso!" });
   } catch (error) {
-    res.status(500).json({ error: error });
-    console.log({error: error})
+    res.status(500).json({ error: error.message });
+    console.log("Erro ao criar o grupo de itens: ", error);
   }
 };
 
@@ -39,40 +53,26 @@ export const updateItemGroup = async (req, res) => {
     const { name, description, depreciation, active } = req.body;
 
     const { id } = req.params;
-    // const itemGroup = await ItemGroup.findById(id).exec();
-    // if (!itemGroup) {
-    //   res.status(204).json({ msg: `Nenhum grupo de itens com esse ID` });
-    // }
+
+    const itemGroupExists = await ItemGroup.getItemGroupById(id);
+    if (!itemGroupExists) {
+      return res.status(404).json({
+        error: "Nenhum grupo de itens encontrado! Forneça um ID válido.",
+      });
+    }
+
     const response = {
       name,
       description,
       depreciation,
       active,
-    }; 
-     await ItemGroup.updateItemGroup(id, response);
-      res
-        .status(201)
-        .json({msg: "Grupo de itens atualizado com sucesso!" });
-    
+    };
+
+    await ItemGroup.updateItemGroup(id, response);
+    res.status(200).json({ msg: "Grupo de itens atualizado com sucesso!" });
   } catch (error) {
-    console.log(error);
-    res.status(500).json({error: "Erro interno no servidor."})
+    console.log("Erro ao atualizar o grupo de itens: ", error);
+    res.status(500).json({ error: error.message });
   }
 };
 
-export const deleteItemGroup = async (req, res) => {
-  try {
-    const { id } = req.params;
-    // const itemGroup = await ItemGroup.findById(id).exec();
-    // if (!itemGroup) {
-    //   res.status(204).json({ msg: `Nenhum centro de custo com esse ID` });
-    // }
-    await ItemGroup.deleteItemGroup(id);
-    res 
-      .status(200)
-      .json({msg: "Grupo de itens deletado com sucesso!" });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({error: "Erro interno do servidor."})
-  }
-};
--- routes/itemGroup.js
+++ routes/itemGroup.js
@@ -1,11 +1,14 @@
 import express from "express";
-import { createItemGroup, deleteItemGroup, getAllItemGroup, updateItemGroup } from "../controllers/itemGroupController.js";
+import {
+  createItemGroup,
+  getAllItemGroup,
+  updateItemGroup,
+} from "../controllers/itemGroupController.js";
 
 const router = express.Router();
 
 router.get("/item-group", getAllItemGroup);
 router.post("/item-group", createItemGroup);
 router.put("/item-group/:id", updateItemGroup);
-router.delete("/item-group/:id", deleteItemGroup);
 
 export default router;
