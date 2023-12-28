5002f21a710e1fffff2e8793f5566dedfa77858f
Eduardo Alves <edu0alves00@gmail.com>
2023-12-28 00:59:35 -0300

    fix: Ajustado a rota que usava o getItemByTag

--- routes/inventory.js
+++ routes/inventory.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { getAllInventory, createInventory, getItemByLocation } from "../controllers/inventoryController.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
-import { getOne } from "../controllers/itemController.js";
+import { getItemByTag } from "../controllers/itemController.js";
 
 const router = express.Router();
 
 router.get("/inventory", getAllInventory);
-router.get("/inventory/item/:tag", getOne);
+router.get("/inventory/item/:tag", getItemByTag);
 router.get("/inventory/location/:location", getItemByLocation);
 // router.post("/inventory", authMiddleware, createInventory);
 router.post("/inventory", createInventory);
--- routes/movement.js
+++ routes/movement.js
@@ -1,13 +1,13 @@
 import express from "express";
 import { createMovement, getAll } from "../controllers/movementController.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
-import { getOne } from "../controllers/itemController.js";
+import { getItemByTag } from "../controllers/itemController.js";
 
 const router = express.Router();
 
 router.post("/movement", authMiddleware, createMovement);
 // router.post("/movement", createMovement);
 router.get("/movement", getAll);
-router.get("/movement/item/:tag", getOne);
+router.get("/movement/item/:tag", getItemByTag);
 
 export default router;
