0f1e8610350c2a4130731814772b1e7b40e658c4
Eduardo Alves <edu0alves00@gmail.com>
2024-01-05 11:04:41 -0300

    fix: Alterado alias para integrar com o front

--- models/Divergences.js
+++ models/Divergences.js
@@ -4,10 +4,10 @@ export const getAllDivergences = async () => {
   const [divergence] = await db.query(
     `
   SELECT 
-    EI.INVENTARIO_IN_ID AS INVENTARIO,
-    GL.LOCALIZACAO_ST_NOME AS LOCALIZACAO, 
-    GU.USUARIO_ST_ALTERNATIVO AS RESPONSAVEL,
-    EI.INVENTARIO_DT_ATUALIZACAO AS DATA_DIVERGENCIA
+    EI.INVENTARIO_IN_ID AS _id,
+    GL.LOCALIZACAO_ST_NOME AS location, 
+    GU.USUARIO_ST_ALTERNATIVO AS responsable,
+    EI.INVENTARIO_DT_ATUALIZACAO AS createdAt
   FROM est_inventario ei 
   LEFT JOIN GLO_LOCALIZACAO GL ON 
     GL.LOCALIZACAO_IN_ID = EI.LOCALIZACAO_IN_ID 
@@ -23,11 +23,11 @@ export const getItemsDivergences = async (inventoryId) => {
   const [itemDivergence] = await db.query(
     `
   SELECT 
-    EII.INVENTARIO_IN_ID,
-    EI.ITEM_IN_ID, 
-    EI.ITEM_ST_NOME,
-    GL.LOCALIZACAO_ST_NOME,
-    EI.ITEM_ST_PATRIMONIO 
+    EII.INVENTARIO_IN_ID as _id,
+    EI.ITEM_IN_ID as itemId, 
+    EI.ITEM_ST_NOME as name,
+    GL.LOCALIZACAO_ST_NOME as location,
+    EI.ITEM_ST_PATRIMONIO as tag
   FROM EST_INVENTARIO_ITEM EII 
   INNER JOIN EST_ITENS EI ON 
     EI.ITEM_IN_ID = EII.ITEM_IN_ID  
@@ -44,10 +44,11 @@ export const getItemsDivergences = async (inventoryId) => {
 export const getCorrectItems = async (inventoryId) => {
   const [itemDivergence] = await db.query(
     `
-    SELECT ET.ITEM_IN_ID, 
-      ET.ITEM_ST_NOME, 
-      ET.ITEM_ST_PATRIMONIO, 
-      ET.LOCALIZACAO_IN_ID  
+    SELECT 
+      ET.ITEM_IN_ID as _id, 
+      ET.ITEM_ST_NOME as name, 
+      ET.ITEM_ST_PATRIMONIO as tag, 
+      ET.LOCALIZACAO_IN_ID as location 
     FROM 
       EST_ITENS ET 
     WHERE 
